878da221c3520998702466561ca8b18a86ba138f
pavibalan <50619426+pavibalan@users.noreply.github.com>
2019-05-14 15:47:10 +0530

    Update sumofnaturalnumbers.py

--- sumofnaturalnumbers.py
+++ sumofnaturalnumbers.py
@@ -1,5 +1,5 @@
-	number=int(input())
-	sum=0
-	for counter in range(1,number+1):
-	   sum+=counter
-	print(sum)
+number=int(input())
+sum=0
+for counter in range(1,number+1)
+	sum+=counter
+print(sum)
